9f1dbdaf1857b4ea2b1869881bad3bdce9252810
Nicole Woodcock <nicole.xox@gmail.com>
2020-12-05 21:41:33 -0800

    add facebook login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,7 +8,10 @@ const options = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    // ...add more providers here
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
   ],
 
   // A database is optional, but required to persist accounts in a database
