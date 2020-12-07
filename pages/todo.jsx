2285ca8c3baf5a88c0bc1e69989d5aae11907364
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-06 20:32:58 -0800

    add reducer, AddTodo, Todo Component and make changes to Todo module

--- pages/todo.jsx
+++ pages/todo.jsx
@@ -1,8 +1,25 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-<h1>Todo</h1>
-  )
+import React, { useReducer } from "react";
+import { initialState, reducer } from "../Reducers/todo.reducer.js";
+import Todo from "../Components/Todo.jsx";
+import AddTodo from "../Components/AddTodo.jsx";
+
+const TodoApp = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (<>
+  <div className={styles.container}>
+    <AddTodo
+      add={text => dispatch({type: "add", text: text})}
+    />
+    {state.todos.map(t => (
+      <Todo
+        key={t.id}
+        todo={t}
+        remove={() => dispatch({type: "remove", id: t.id})}
+        edit={text => dispatch({type: "edit", id: t.id, text: text})}
+      />
+    ))}
+    </div>
+  </>);
 }
+export default TodoApp;
