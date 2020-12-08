fcc53b616c90d8b43e6319d15be3a177b03877fa
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-08 12:34:00 -0800

    add hiddenSettings page

---
+++ pages/hiddenSettings.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function hiddenSettings() {
+  return (
+    <div>
+  <p>ListKeeper is disguised as an unsuspecting todo list app so if you are in a dangerous situation and you need to be stealthy in calling for help, ListKeeper can keep you safe. </p>
+
+  <p>When you input a todo on ListKeeper the search bar will autocomplete into a known phrase e.g. Help. When you press the ADD button listKeeper sends a coded message to your trusted contacts via SMS and Email. If you need immediate help you can always access this secret menu and get connected to 911 via the Emergency button </p>
+  
+      <button>Tutorial</button>
+      <button>Emergency</button>
+    </div>
+  )
+}
+
+export default hiddenSettings
