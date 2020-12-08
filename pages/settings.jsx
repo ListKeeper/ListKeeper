30dc0f1d2c164c0cdc2319545fe2be4d655a7a3b
Nicole Woodcock <nicole.xox@gmail.com>
2020-12-08 10:19:06 -0800

    add fix to mongodb dependancy error
    https://github.com/nextauthjs/next-auth/issues/552#issuecomment-671662283

--- pages/settings.jsx
+++ pages/settings.jsx
@@ -1,8 +1,40 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useContext } from "react";
+import { StoreContext } from "../Reducers/reducer.js";
+import Todo from "../Components/Todo.jsx";
+import AddTodo from "../Components/AddTodo.jsx";
+import styles from "../styles/Todo.module.css";
 
-export default function Home() {
+// Any component can grab and use state & dispatch
+const Settings = () => {
+  const { state, dispatch } = useContext(StoreContext);
+  console.log(state);
   return (
-<h1>Settings</h1>
-  )
-}
+    <>
+      <div className={styles.container}>
+        <h1>Settings</h1>
+        {state.settings.emergencyContacts.map((c) => (
+          <Todo
+            key={c.id}
+            todo={c}
+            remove={() =>
+              dispatch({ type: "remove-contact", payload: { id: c.id } })
+            }
+            edit={(phoneNumber) =>
+              dispatch({
+                type: "edit-contact",
+                payload: { id: c.id, phoneNumber },
+              })
+            }
+          />
+        ))}
+        <AddTodo
+          add={(phoneNumber) =>
+            dispatch({ type: "add-contact", payload: { phoneNumber } })
+          }
+        />
+      </div>
+    </>
+  );
+};
+
+export default Settings;
