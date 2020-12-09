d83992f37ef71a708db2e64ff3f306dec70e72fb
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-08 17:40:56 -0800

    add components and tutorial reducer for tutorial page

--- pages/tutorial.jsx
+++ pages/tutorial.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useContext, useReducer } from 'react'
 import Styles from "../styles/Tutorial.module.css"
 import { Button } from "@material-ui/core";
+import {initialState, reducer} from "../Reducers/TutorialReducer.js"
 import Link from "next/link"
+import Todo from "../Components/Todo.jsx";
+import AddTodo from "../Components/AddTodo.jsx";
+import styles from "../styles/Tutorial.module.css"
 
 function tutorial() {
+  // Any component can grab and use state & dispatch
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
+    <>
     <div>
     <div>
       Tutorial
     </div>
+    <div>
+
+    </div>
     <Button><Link href="/hiddenSettings">Back</Link></Button>
     </div>
+
+    <div className={styles.container}>
+        <h1>Todos</h1>
+        {state.todos.map((t) => (
+          <Todo
+            key={t.id}
+            todo={t}
+            remove={() => dispatch({ type: "remove-todo", id: t.id })}
+            edit={(text) =>
+              dispatch({ type: "edit-todo", id: t.id, text: text })
+            }
+          />
+        ))}
+        <AddTodo add={(text) => dispatch({ type: "add-todo", text: text })} />
+        <div>
+          <Link href="/"><button>Back</button></Link>
+        </div>
+      </div>
+    </>
   )
 }
 
