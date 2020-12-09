b8e5c5329e0927ac15a28a24ab729c260f80363d
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-08 16:51:20 -0800

    fix up api to lower case and add the phrases object

--- pages/api/tutorialCommands.js
+++ pages/api/tutorialCommands.js
@@ -4,14 +4,17 @@ export default async (req, res) => {
   const session = await getSession({ req })
   if (session) {
   res.statusCode = 200
-  res.json({ 
+  res.json({
+    commands: {
     Help: 'I am in a dangerous situation and I require help.',
     Stranger: "I think I am being followed.",
     Violence: "Another person I am near to is growing increasingly angry or violent.",
     Escalating: "There is a situation with more or one people and it is escalating.",
     Threatened: "I have been verbally threatened or abused.",
-    Medical: "I am having a medical emergency."
-
+    Medical: "I am having a medical emergency.",
+    Allclear: "I am okay, situation has resolved itself", 
+    },
+    phrases: { random : ['Buy Milk', 'Wash Laundry', 'Buy Birthday Gift', 'Clean Bathroom', 'Write Work Email', 'Family Check-In', 'Check Mail', 'Book Vacation', 'Sweep Floor', 'Organize Drawer' ]}
  })
   } else {
     res.status(401)
