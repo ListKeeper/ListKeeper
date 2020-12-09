886c27624b0104afbd8bd1e803545e1453203794
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-09 10:39:42 -0800

    add material UI lab as dependency and implement autocomplete

--- pages/tutorial.jsx
+++ pages/tutorial.jsx
@@ -1,43 +1,50 @@
-import React, { useContext, useReducer, useState, useEffect } from 'react'
-import Styles from "../styles/Tutorial.module.css"
+import React, { useContext, useReducer, useState, useEffect } from "react";
+import Styles from "../styles/Tutorial.module.css";
 import { Button } from "@material-ui/core";
-import {initialState, reducer} from "../Reducers/TutorialReducer.js"
-import Link from "next/link"
+import { initialState, reducer } from "../Reducers/TutorialReducer.js";
+import Link from "next/link";
 import Todo from "../Components/Todo.jsx";
 import AddTodo from "../Components/AddTodo.jsx";
-import styles from "../styles/Tutorial.module.css"
-
+import styles from "../styles/Tutorial.module.css";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 const tutorial = () => {
-  const fetchData = async () => {
-    const res = await fetch('api/tutorialCommands');
-    const json = await res.json();
-    setData(json);
-  };
-  
-  useEffect(() => {
-    fetchData();
-    console.log(data)
-  }, []);
-  // Any component can grab and use state & dispatch
+  const data = [
+    {
+      label: "Help",
+      message: "I am in a dangerous situation and I require help.",
+    },
+    { label: "Stranger", message: "I think I am being followed." },
+    {
+      label: "Violence",
+      message:
+        "Another person I am near to is growing increasingly angry or violent.",
+    },
+    {
+      label: "Escalating",
+      message:
+        "There is a situation with more or one people and it is escalating.",
+    },
+    {
+      label: "Threatened",
+      message: "I have been verbally threatened or abused.",
+    },
+    { label: "Medical", message: "I am having a medical emergency." },
+    { label: "Allclear", message: "I am okay, situation has resolved itself" },
+  ];
+
+
   const [state, dispatch] = useReducer(reducer, initialState);
-  const [data, setData] = useState([]);
-const [query, setQuery] = useState('');
-const [heroes, setHeroes] = useState([]);
+
   return (
     <>
-    <div>
-    <div>
-      Tutorial
-    </div>
-    <div>
+      <div>
+        <div>Tutorial</div>
+      </div>
 
-    </div>
-    <Button><Link href="/hiddenSettings">Back</Link></Button>
-    </div>
-
-    <div className={styles.container}>
-        <h1>Todos</h1>
+      <div className={styles.container}>
+        <h1>Todos (Tutorial)</h1>
         {state.todos.map((t) => (
           <Todo
             key={t.id}
@@ -48,14 +55,25 @@ const [heroes, setHeroes] = useState([]);
             }
           />
         ))}
-        <AddTodo add={(text) => dispatch({ type: "add-todo", text: text })} />
-      <div id="messageBox" className={styles.messagebox}></div>
+
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={data}
+          getOptionLabel={(option) => option.label}
+          style={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Input Command" />
+          )}
+        />
+
         <div>
-          <Link href="/"><button>Back</button></Link>
+          <Link href="/hiddenSettings">
+            <button>Back</button>
+          </Link>
         </div>
       </div>
     </>
-  )
-}
-
-export default tutorial
+  );
+};
+export default tutorial;
